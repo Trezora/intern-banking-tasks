097e5f01f50f644d96ec4c92490935153a74ecdf
Trezora <bekabuliskeria03@gmail.com>
2025-04-08 11:12:40 +0400

    changes

--- BankingApp/obj/Debug/net8.0/BankingApp.AssemblyInfo.cs
+++ BankingApp/obj/Debug/net8.0/BankingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49091d01878eeafddd4e285e5dd330939331fa6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1154fe6c9405e2d4e65286128ea247eaddc64eea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
