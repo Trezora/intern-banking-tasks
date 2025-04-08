ce4b5125a4e3fc95b79e8be1d3d54ce795be12cf
Trezora <bekabuliskeria03@gmail.com>
2025-04-08 17:26:57 +0400

    Fixed enviroment for Project

---
+++ BankingApp.Tests/BankCustomer/BankCustomerTests.cs
@@ -0,0 +1,13 @@
+using BankingApp.BankCustomer;
+using Xunit;
+
+namespace BankingApp.Tests.BankCustomer
+{
+    public class BankCustomerTests
+    {
+        [Fact]
+        public void TestCustomerName()
+        {
+        }
+    }
+}
