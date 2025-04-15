02a6647f192df05f977f796ce9865127d709f919
Trezora <bekabuliskeria03@gmail.com>
2025-04-15 17:46:06 +0400

    Added CLI part

--- BankingApp/BankingApp.Domain/BankAccount/BankAccount.cs
+++ BankingApp/BankingApp.Domain/BankAccount/BankAccount.cs
@@ -12,18 +12,18 @@ namespace BankingApp.Domain.Accounts
 
         public BankAccount(Customer customer, Money initialDeposit)
         {   
-            AccountId = new Guid();
+            AccountId = Guid.NewGuid();
             Customer = customer;
             Balance = initialDeposit;
             AccountNumber = AccountNumber.GenerateAccountNumber();
         }
         public string PrintAccountSummary()
         {
-            return $"Customer Summary:\n" +
+            return $"Account Summary:\n" +
                    $"- ID: {AccountId}\n" +
-                   $"- Customer: {Customer.GetCustomerSummary()}\n" +
-                   $"- Account Number: {AccountNumber.ToString()}" +
-                   $"- Balance: {Balance.ToString()}";
+                   $"- Customer:\n {Customer.GetCustomerSummary()}\n" +
+                   $"- Account Number: {AccountNumber.ToString()}\n" +
+                   $"- Balance: {Balance.ToString()}\n";
 
         }
     }
